f8466d6f219849fc003f4258f8a87ee07486976c
Jurgo Boemo <jurgo.boemo@gmail.com>
2017-02-26 16:28:07 +0000

    add logo.svg. inline way

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+import Logo from './Logo';
 // import './App.css';
 
 class App extends Component {
@@ -7,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
+          <Logo />
           <h2>Welcome to React yollo</h2>
         </div>
         <p className="App-intro">
